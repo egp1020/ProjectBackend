686b5015f98eb403859bf8afe04c29ab368e2858
Estefanía Garcés Pérez <Estefania.perez.0110@gmail.com>
2021-09-30 16:15:42 -0500

    Changes: organizing routes of product by controllers. EGP.

--- app/product/routes.py
+++ app/product/routes.py
@@ -1,54 +1,58 @@
 from flask import jsonify, request
 from flask import Blueprint
-from .product import product
+from app.product.controller import ProductController
 
 
 bpproduct = Blueprint('bpproduct', __name__, template_folder='templates')
-
+controller = ProductController()
 
 @bpproduct.route('/product/', methods=["GET"])
 def get():
-    products = product().get()
+    products = controller.getProductAll()
     return jsonify(products)
 
 
 @bpproduct.route("/product/<id>/", methods=["GET"])
 def getProduct(id):
-    get_product = product().get_by_id(id)
-    return jsonify(get_product)
+    product = controller.getProduct(id)
+    return jsonify(product)
 
 
 @bpproduct.route("/product/", methods=["POST"])
-def create():
-    productDetail = request.get_json()
-    photo = productDetail["photo"]
-    description = productDetail["description"]
-    category = productDetail["category"]
-    quantity = productDetail["quantity"]
-    stock = productDetail["stock"]
-    price = productDetail["price"]
-    barcode = productDetail["barcode"]
-    tax = productDetail["tax"]
-    result = product().create(photo, description, category, quantity, stock, price, barcode, tax)
-    return jsonify(result)
+def createProduct():
+    product = {
+        'photo':request.json["photo"],
+        'description': request.json["description"],
+        'category': request.json["category"],
+        'quantity': request.json["quantity"],
+        'stock': request.json["stock"],
+        'price': request.json["price"],
+        'tax': request.json["tax"],
+        'barcode': request.json["barcode"]
+    }
+
+    products = controller.insertProduct(product)
+    return products
 
 
 @bpproduct.route('/product/<id>/', methods=["PUT"])
-def update(id):
-    productDetail = request.get_json()
-    photo = productDetail["photo"]
-    description = productDetail["description"]
-    category = productDetail["category"]
-    quantity = productDetail["quantity"]
-    stock = productDetail["stock"]
-    price = productDetail["price"]
-    barcode = productDetail["barcode"]
-    tax = productDetail["tax"]
-    result = product().update(id, photo, description, category, quantity, stock, price, barcode, tax)
-    return jsonify(result)
+def updateProduct(id):
+    product = {
+        'photo':request.json["photo"],
+        'description': request.json["description"],
+        'category': request.json["category"],
+        'quantity': request.json["quantity"],
+        'stock': request.json["stock"],
+        'price': request.json["price"],
+        'tax': request.json["tax"],
+        'barcode': request.json["barcode"]
+    }
+
+    products = controller.updateProduct(product)
+    return products
 
 
 @bpproduct.route('/product/<id>/', methods=["PUT"])
-def delete(id):
-    result = product().delete(id)
-    return jsonify(result)
+def deleteProduct(id):
+    products = controller.deleteProduct(id)
+    return products
