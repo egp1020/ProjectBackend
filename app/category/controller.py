4b988bd65f340299a09e3619185c833c6e622322
Estefanía Garcés Pérez <Estefania.perez.0110@gmail.com>
2021-09-30 09:10:39 -0500

    ADD: category controller

--- app/category/controller.py
+++ app/category/controller.py
@@ -6,12 +6,12 @@ class CategoryController:
         categories = category.query.all()
         category = []
 
-        for line in categories:
+        for element in categories:
             category.append({
-                'id':line.id,
-                'photo':line.photo,
-                'name':line.name,
-                'description':line.description
+                'id':element.id,
+                'photo':element.photo,
+                'name':element.name,
+                'description':element.description
             })
         return category
 
@@ -36,3 +36,40 @@ class CategoryController:
         photo = category["photo"]
         name = category["name"]
         description = category["description"]
+        category = Category.query.filter_by(id = id).first()
+        if category is not None:
+            data = Category(photo, name, description)
+            db.session.add(data)
+            db.session.commit()
+
+            text = "El registro se hizo con éxito"
+        else:
+            text = "El registro no se logro."
+        return text
+
+    def updateCategory(self, category):
+        photo = category["photo"]
+        name = category["name"]
+        description = category["description"]
+        category = Category.query.filter_by(id=id).first()
+        if category is not None:
+            category.photo = photo
+            category.name = name
+            category.description = description
+            db.session.add(category)
+            db.session.commit()
+            text = "La categoría se actualizo correctamente"
+        else:
+            text = "La actualización no se pudo realizar con éxito"
+        return text
+
+
+    def deleteCategory(self, id):
+        category = Category.query.filter_by(id=id).first()
+        if category is not None:
+            db.session.delete(category)
+            db.session.commit()
+            text = "La categoría ha sido eliminada con éxito."
+        else:
+            text = "La categoría no se pudo eliminar."
+
