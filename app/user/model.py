1caf905273714e5dde3a1e8ffe7bc44de7bd9b48
Estefanía Garcés Pérez <1020egp@gmail.com>
2021-11-01 16:35:45 -0500

    Add: User model

---
+++ app/user/model.py
@@ -0,0 +1,19 @@
+from app import db
+
+class User(db.Model):
+    __tablename__="user"
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(15), nullable=False, unique=True)
+    email=db.Column(db.String(80), nullable=False)
+    password=db.Column(db.Text, nullable=False)
+
+    def __init__(self, username, email, password):
+        self.username=username
+        self.email=email
+        self.password=password
+
+    def __str__(self):
+        return f"User: {self.username}"
+
+    def __repr__(self):
+        return f"<User {self.username}>"
